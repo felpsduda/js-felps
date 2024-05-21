385595abd0e831c237fb2f9dbd0a49460b692bb4
felpsduda <00001114831980sp@al.educacao.sp.gov.br>
2024-05-21 14:48:46 -0300

    arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(400, 400);
+    background("red");
+  }
+  
+  function draw() {
+    stroke("white");
+    fill("black")
+    
+    if(mouseIsPressed){
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
